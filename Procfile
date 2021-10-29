583b906287f2d9e25aaf119da3c4368d8597ec73
imai0212 <imai021202@gmail.com>
2021-10-29 16:22:21 +0900

    linebot_echo.pyの修正

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn source.linebot_echo:app --host=0.0.0.0 --port=${PORT:-5000}
+web:uvicorn source.linebot_echo:app --host=0.0.0.0 --port=${PORT:-5000}
--- source/linebot_echo.py
+++ source/linebot_echo.py
@@ -18,11 +18,11 @@ app = FastAPI()
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 # 未セット時のエラー処理
-if channel_secret is not None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable')
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
-if  channel_access_token is not None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable')
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
 # 正常セット時の処理
@@ -31,17 +31,11 @@ handler = WebhookHandler(channel_secret)
 
 @app.post("/callback")
 async def callback(request: Request):
-    """
-    LINE PlatformのRequestを処理します
-    Args:
-        request: Message Webhook event object which contains the sent message.
-        https://developers.line.biz/en/reference/messaging-api/#message-event
-    """
-
+    # LINE PlatformのRequest処理
     # リクエストヘッダーから署名検証のための値を取得
-    signature = request.headers('X-Line-Signature')
+    signature = request.headers['X-Line-Signature']
     # リクエストボディを取得
-    body = (await request.body()).decode('utf-8')
+    body = (await request.body()).decode("utf-8")
     # 署名を検証し、問題なければhandleに定義されている関数を呼び出す
     try:
         handler.handle(body, signature)
@@ -50,20 +44,8 @@ async def callback(request: Request):
         raise HTTPException(status_code=400, detail="Signature verification failed")
     return 'OK'
 
-# @handler.addメソッドの第一引数には、LINE Platformからの"Webhook Event Objects"を指定します。
-# https://developers.line.biz/en/reference/messaging-api/#webhook-event-objects
-#
-# Eventはlinebot.modelsに定義されていて以下の種類があります。
-# 　TextMessage,
-# 　ImageMessage,
-# 　VideoMessage,
-# 　AudioMessage,
-# 　LocationMessage,
-# 　StickerMessage,
-# 　FileMessage
-# handler.addメソッドはメッセージイベントの場合にテキストや画像のようなメッセージの
-# 内容でも処理を分けることができます。
-
+# @handler.addメソッド第一引数:
+# LINE Platformからの"Webhook Event Objects"を指定
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event: MessageEvent) -> None:
     line_bot_api.reply_message(
