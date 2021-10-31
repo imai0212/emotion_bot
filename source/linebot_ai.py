859d0ff849ce9b0586ba852065f4197d74e1773c
imai0212 <imai021202@gmail.com>
2021-10-31 14:38:14 +0900

    テスト

--- source/linebot_ai.py
+++ source/linebot_ai.py
@@ -5,7 +5,7 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 from dotenv import load_dotenv
-from source import client
+from . import client
 
 
 load_dotenv(verbose=True)
